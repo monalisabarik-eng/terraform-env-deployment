65221baaa73aaa1e2be425197be435fefdd09421
Monalisa <monalisa.barik@nttdata.com>
2025-11-05 16:57:59 +0530

    added prod storage account

--- envs/prod/backend.tf
+++ envs/prod/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "rg-terraform-state"
-    storage_account_name = "tfstateaccount"
+    storage_account_name = "tfstateaccounttf"
     container_name       = "tfstate"
     key                  = "prod.terraform.tfstate"
   }
